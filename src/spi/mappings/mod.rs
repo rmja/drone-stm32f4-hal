19700b6461f10819204134b930d07ae1c4b09d55
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2020-12-14 21:09:44 +0100

    Implement all spi1 and spi2 mappings

--- src/spi/mappings/mod.rs
+++ src/spi/mappings/mod.rs
@@ -1,5 +1,3 @@
-#[cfg(any(stm32_mcu = "stm32f427", stm32_mcu = "stm32f429",))]
-mod f427_f429;
+mod spi12;
 
-#[cfg(any(stm32_mcu = "stm32f427", stm32_mcu = "stm32f429",))]
-pub use self::f427_f429::*;
+pub use self::spi12::*;
