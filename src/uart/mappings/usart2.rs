bab8921b7bc8cb230d6acce101fca06adfc3b20e
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2020-12-15 08:13:35 +0100

    Fix usart2 pin mappings

--- src/uart/mappings/usart2.rs
+++ src/uart/mappings/usart2.rs
@@ -11,7 +11,31 @@ rx_drv_init!(Usart2, Dma1Ch5, DmaStCh4);
 tx_drv_init!(Usart2, Dma1Ch6, DmaStCh4);
 
 pin_impl!(RxPinExt for UartPins<drone_stm32_map::periph::uart::Usart2, ...>.rx, GpioA3, AlternateMode<PinAf7>; Undefined, Tx => Defined, Tx);
+#[cfg(any(
+    stm32_mcu = "stm32f401",
+    stm32_mcu = "stm32f405",
+    stm32_mcu = "stm32f407",
+    stm32_mcu = "stm32f411",
+    stm32_mcu = "stm32f412",
+    stm32_mcu = "stm32f413",
+    stm32_mcu = "stm32f427",
+    stm32_mcu = "stm32f429",
+    stm32_mcu = "stm32f446",
+    stm32_mcu = "stm32f469",
+))]
 pin_impl!(RxPinExt for UartPins<drone_stm32_map::periph::uart::Usart2, ...>.rx, GpioD6, AlternateMode<PinAf7>; Undefined, Tx => Defined, Tx);
 
 pin_impl!(TxPinExt for UartPins<drone_stm32_map::periph::uart::Usart2, ...>.tx, GpioA2, AlternateMode<PinAf7>; Tx, Undefined => Tx, Defined);
+#[cfg(any(
+    stm32_mcu = "stm32f401",
+    stm32_mcu = "stm32f405",
+    stm32_mcu = "stm32f407",
+    stm32_mcu = "stm32f411",
+    stm32_mcu = "stm32f412",
+    stm32_mcu = "stm32f413",
+    stm32_mcu = "stm32f427",
+    stm32_mcu = "stm32f429",
+    stm32_mcu = "stm32f446",
+    stm32_mcu = "stm32f469",
+))]
 pin_impl!(TxPinExt for UartPins<drone_stm32_map::periph::uart::Usart2, ...>.tx, GpioD5, AlternateMode<PinAf7>; Tx, Undefined => Tx, Defined);
